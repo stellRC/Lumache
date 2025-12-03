edeedd93d07f4c51728fe8f977ff250d3cd5781c
StellRC <stellaffarce@gmail.com>
2025-12-03 13:10:54 -0800

    update git pages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,8 @@ release = '0.1'
 extensions = ['sphinx.ext.duration',
               'sphinx.ext.doctest',
               'sphinx.ext.autodoc',
-              'sphinx.ext.autosummary',]
+              'sphinx.ext.autosummary',
+              'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
